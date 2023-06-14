da779d06d43ac6b761b11be8ea65591384f3e902
Альберт <albert1702@ya.ru>
2023-06-14 13:14:19 +0300

    6. Правила Checkstyle [#504981  [#504981 #422547]]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,19 +2,19 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     private int sizeOfEmpty = 10;
-    String name;
+    private String name;
 
     Broken() { }
 
-    void echo() { }
+    public void echo() { }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) { }
+    public void method(int a, int b, int c, int d, int e, int f, int g) { }
 
     public String surname;
 
