6c687af9876cddc677c4d31f8df4ee9d77688f4e
Альберт <albert1702@ya.ru>
2023-06-20 16:12:13 +0300

    3.6. Реализация класса StartUI. Вывод заявок по имени [#500749  [#500749 #423808]]

--- src/main/java/ru/job4j/poly/FileStore.java
+++ src/main/java/ru/job4j/poly/FileStore.java
@@ -10,11 +10,9 @@ public class FileStore implements Store {
     }
 
     public void save(String data) {
-        //Тут реализация хранения данные в файле
     }
 
     public String read() {
-        // Тут чтение файла
         return null;
     }
 }
