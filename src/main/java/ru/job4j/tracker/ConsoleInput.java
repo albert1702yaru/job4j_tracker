dda7ace37d3b7ad6cd5aa36d25c3afa9ce727910
Альберт <albert1702@ya.ru>
2023-06-21 12:27:55 +0300

    6.1. Разрыв зависимости StartUI от Scanner. [#181778  [#181778 #424212]]

--- src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -14,6 +14,6 @@ public class ConsoleInput implements Input {
 
     @Override
     public int askInt(String question) {
-        return Integer.parseInt(askStr(question));
+        return askInt(question);
     }
 }
