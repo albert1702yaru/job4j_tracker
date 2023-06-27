6f2d7402fb18a33c91abce3d96f63198501a35c1
Альберт <albert1702@ya.ru>
2023-06-27 06:08:07 +0300

    0. Map, HashMap [#212652  [#212652 #425460]]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
 
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("albert1702@ya.ru", "Иванов Иван Иванович");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
         map.put("albert1702@ya.ru", "Исманский Альберт Анатольевич");
         for (String key : map.keySet()) {
             String value = map.get(key);
