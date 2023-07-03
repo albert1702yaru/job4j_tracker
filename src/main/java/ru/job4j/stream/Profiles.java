5c4f022c89a98b035abdf2b2170d0c27986c3744
Альберт <albert1702@ya.ru>
2023-07-03 16:03:58 +0300

    6. Список адресов [#110062  [#110062]]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
+
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
